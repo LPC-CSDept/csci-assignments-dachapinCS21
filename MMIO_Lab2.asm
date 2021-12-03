48a56449a3e88d7014a0c0b2037b6000f0abfc6c
dachapinCS21 <94938447+dachapinCS21@users.noreply.github.com>
2021-12-03 10:22:17 -0800

    Update MMIO_Lab2.asm
    Made the code be able to handle cases where $t9 is not just 2.

--- MMIO_Lab2.asm
+++ MMIO_Lab2.asm
@@ -8,7 +8,7 @@
 	.text
 	.globl	main
 main:
-	li	$t9, 2			# $t9 = 2 digits count
+	li	$t9, 2			# $t9 Set to number of digits to be read
 	lui	$t0, 0xFFFF
 	
 rd_wait:
@@ -17,17 +17,24 @@ rd_wait:
 	beqz	$t1, rd_wait		# if not ready(0) restart loop, if ready(1) continue
 	lw	$s0, 4($t0)		# read from input device
 	
-	sub	$s0, $s0, 48		# $s0 = $v0 - 48 (ASCII code conversion to decimal)
+	sub	$s0, $s0, 48		# $s0 -= 48 Convert from ASCII to decimal
 	sub	$t9, $t9, 1		# decrement digit count
-	beqz	$t9, print		# move to print if last digit input
-	
-	mul	$s1, $s0, 10		# Multiply decimal value of first digit by 10
-	b	rd_wait			# Return to the start of the loop
-	
-	
+	bnez	$t9, m_start		# branch to m_start if not the last digit
+	b	dg_add			# branch to dig_add if the last digit
+
+m_start:
+	or	$t8, $t9, $zero		# $t8 = $t9
+pl_mu:	
+	mul	$s0, $s0, 10		# Multiply decimal value of first digit by 10
+	sub	$t8, $t8, 1
+	bnez	$t8, pl_mu		# retiterate pl_mu unless $t8 = 0
+
+dg_add:
+	add	$s1, $s1, $s0		# Add digit value to $s1
+	bnez	$t9, rd_wait		# branch to rd_wait if not the last digit
 	
 print:
-	add	$a0, $s1, $s0		# Add both digit values together
+	or	$a0, $s1, $zero		# Set $a0 to $s1
 	li	$v0, 1			# System call code to print integer
 	syscall
 	
