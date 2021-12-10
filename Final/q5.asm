7a468ea8832b64e66b35b13f6a583a77acdbb0af
Dan Chapin <dan.chapin@lcorthopedicsinnovation.com>
2021-12-10 08:53:52 -0800

    Added additional comments

--- Final/q5.asm
+++ Final/q5.asm
@@ -15,15 +15,15 @@ main:
         lui     $t9, 0xFFFF         # $t9 = Memory location of the start of the MMIO section(Receiver Control) 
 rd_wait:
         lw      $t1, 0($t9)         # Load Receiver Control to $t1
-        andi    $t1, $t1, 1         # Clear all bits of Receiver Control besides the LSB
+        andi    $t1, $t1, 1         # Clear all bits of Receiver Control besides the LSB(Ready Bit)
         beqz    $t1, rd_wait        # If not ready(0) restart rd_wait, if ready(1) continue onto calculation
-        lw      $t2, 4($t9)         # Read data in Receiver Data to $t2
+        lw      $t2, 4($t9)         # Read data from Receiver Data to $t2
 
-        sub     $t2, $t2, 48        # Subtract $t2 by 48 to convert from ASCII to int
-        mul     $t2, $t2, $t0       # Multiply $t2 by the place value factor in $t0
-        div     $t0, $t0, 10        # Divide $t0 to decrease place value factor to the next place
+        sub     $t2, $t2, 48        # Subtract $t2 by 48 to convert from ASCII to int(Example: '1' = 49 - 48 = 1)
+        mul     $t2, $t2, $t0       # Multiply $t2 by the place value factor in $t0(9 * 100 = 900, 5 * 10 = 50, 3 * 1 = 3)
+        div     $t0, $t0, 10        # Divide $t0 to decrease place value factor to the next place(100/10 = 10, 10/10 = 1, 1/10 = 0)
 
-        add     $t3, $t3, $t2       # Add current digits value to total($t3)
+        add     $t3, $t3, $t2       # Add current digits value to $t3(900 + 50 + 3)
         bnez    $t0, rd_wait        # If not on the last place(0) poll for the next digit
 
 print:
