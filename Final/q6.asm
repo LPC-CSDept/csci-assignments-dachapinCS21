afc42d657cffb37291b08fc3f22630aea868bbc8
Dan Chapin <dan.chapin@lcorthopedicsinnovation.com>
2021-12-10 10:02:21 -0800

    Additional comments

--- Final/q6.asm
+++ Final/q6.asm
@@ -14,15 +14,16 @@ nl:     .asciiz "\n"                # New line
         .globl  main
 main:
         mfc0    $a0, $12            # Read the Status Register
-        ori     $a0, 0xFF11         # Enable all interrupts
+        ori     $a0, 0xFF11         # Bit pattern to enable all interrupts
         mtc0    $a0, $12            # Copy new pattern, with interrupts enabled back to the Status Register
 
         lui     $t9, 0xFFFF         # $t9 = Memory location of the start of the MMIO section(Receiver Control) 
-        ori     $a0, $zero, 2       # Enable keyboard interrupt
+        ori     $a0, $zero, 2       # Bit pattern to enable keyboard interrupt
         sw      $a0, 0($t9)         # Store new pattern to Receiver Control to enable interrupts
 
 loop:
         j       loop                # Infinite loop until 'q' is reached
+                                    # If keyboard interrupt detected will go to Kernel Text
 
         .ktext  0x80000180          # Start of Kernel text
         sw      $v0, s1             # Registers for use in the kernel
@@ -31,7 +32,7 @@ loop:
         mfc0    $k0, $13            # Read Cause Register to $k0
         srl     $a0, $k0, 2         # Bit 0 and 1 of Cause Register are blank
         andi    $a0, $a0, 0x1F      # Only leave 5 bits that represeent Exception Code
-        bnez    $a0, done           # If exception code is skip to done
+        bnez    $a0, done           # If exception code is 0 branch to done
 
         lui     $v0, 0xFFFF         # $v0 = Memory location of the start of the MMIO section(Receiver Control) 
         lw      $a0, 4($v0)         # Read data from Receiver Data to $a0
