57cb0c2bca26e39a68dd6123b265bde0dba8cb52
Dan Chapin <dan.chapin@lcorthopedicsinnovation.com>
2021-12-10 09:47:12 -0800

    Added check if $a0 == 'q' ASCII if so exit

--- Final/q6.asm
+++ Final/q6.asm
@@ -12,38 +12,43 @@ s2:     .word   11
         .text
         .globl  main
 main:
-        mfc0    $a0, $12        # Read the Status Register
-        ori     $a0, 0xFF11     # Enable all interrupts
-        mtc0    $a0, $12        # Copy new pattern, with interrupts enabled back to the Status Register
+        mfc0    $a0, $12            # Read the Status Register
+        ori     $a0, 0xFF11         # Enable all interrupts
+        mtc0    $a0, $12            # Copy new pattern, with interrupts enabled back to the Status Register
 
-        lui     $t9, 0xFFFF     # $t9 = Memory location of the start of the MMIO section(Receiver Control) 
-        ori     $a0, $zero, 2   # Enable keyboard interrupt
-        sw      $a0, 0($t9)     # Store new pattern to Receiver Control to enable interrupts
+        lui     $t9, 0xFFFF         # $t9 = Memory location of the start of the MMIO section(Receiver Control) 
+        ori     $a0, $zero, 2       # Enable keyboard interrupt
+        sw      $a0, 0($t9)         # Store new pattern to Receiver Control to enable interrupts
 
 loop:
-        j       loop            # Infinite loop until 'q' is reached
+        j       loop                # Infinite loop until 'q' is reached
 
-        .ktext  0x80000180      # Start of Kernel text
-        sw      $v0, s1         # Set $v0 to 10
-        sw      $a0, s2         # Set $a0 to 11
-        eret
-        mfc0    $k0, $13        # Read Cause Register to $k0
-        srl     $a0, $k0, 2     # Bit 0 and 1 of Cause Register are blank
-        andi    $a0, $a0, 0x1F  # Only leave 5 bits that represeent Exception Code
-        bnez    $a0, done       # If exception code is skip to done
+        .ktext  0x80000180          # Start of Kernel text
+        sw      $v0, s1             # Set $v0 to 10
+        sw      $a0, s2             # Set $a0 to 11
+        
+        mfc0    $k0, $13            # Read Cause Register to $k0
+        srl     $a0, $k0, 2         # Bit 0 and 1 of Cause Register are blank
+        andi    $a0, $a0, 0x1F      # Only leave 5 bits that represeent Exception Code
+        bnez    $a0, done           # If exception code is skip to done
 
-        lui     $v0, 0xFFFF     # $v0 = Memory location of the start of the MMIO section(Receiver Control) 
-        lw      $a0, 4($v0)     # Read data from Receiver Data to $a0
-        li      $v0, 4          # System Code to print string
+        lui     $v0, 0xFFFF         # $v0 = Memory location of the start of the MMIO section(Receiver Control) 
+        lw      $a0, 4($v0)         # Read data from Receiver Data to $a0
+        bne     $a0, 113, print     # Branch to print if not equal to ASCII code for 'q'
+        li      $v0, 10             # System Code to exit
+        syscall
+
+print:
+        li      $v0, 4              # System Code to print string
         syscall
 
 done:
-        lw      $v0, s1         # Restore $v0
-        lw      $a0, s2         # Restore $a0
-        mtc0    $zero, $13      # Clear Cause Register
-        mfc0    $k0, $12        # Set Status Register
-        ori     $k0, 0x11       # Reenable interrupts
-        mtc0    $k0, $12        # Copy to Status Register
-        eret                    # Return to Exception Program Counter
+        lw      $v0, s1             # Restore $v0
+        lw      $a0, s2             # Restore $a0
+        mtc0    $zero, $13          # Clear Cause Register
+        mfc0    $k0, $12            # Set Status Register
+        ori     $k0, 0x11           # Reenable interrupts
+        mtc0    $k0, $12            # Copy to Status Register
+        eret                        # Return to Exception Program Counter
 
 
