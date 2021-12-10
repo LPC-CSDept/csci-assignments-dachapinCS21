84e67a4b7dce69ba5a9ed03780fa9cfa2b455032
Dan Chapin <dan.chapin@lcorthopedicsinnovation.com>
2021-12-10 09:06:03 -0800

    Enabled interrupts

--- Final/q6.asm
+++ Final/q6.asm
@@ -5,3 +5,14 @@
 # CS21 Final Q6
 # Take input by user and print it in the console until 'q' is typed using an interrupt handler
 #
+
+        .text
+        .globl  main
+main:
+        mfc0    $a0, $12        # Read the Status Register
+        ori     $a0, 0xFF11     # Enable all interrupts
+        mtc0    $a0, $12        # Copy new pattern, with interrupts enabled back to the Status Register
+
+        lui     $t9, 0xFFFF     # $t9 = Memory location of the start of the MMIO section(Receiver Control) 
+        ori     $a0, $zero, 2   # Enable keyboard interrupt
+        sw      $a0, 0($t9)     # Store new pattern to Receiver Control to enable interrupts
